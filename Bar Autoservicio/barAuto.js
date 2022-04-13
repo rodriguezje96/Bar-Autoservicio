af213ad5f34d9b3a3bcc8d164b4c021c1882fa01
rodriguezje96 <rodriguez.je96@gmail.com>
2022-04-13 15:50:44 -0300

    Eventos

--- Bar Autoservicio/barAuto.js
+++ Bar Autoservicio/barAuto.js
@@ -2,15 +2,22 @@
 const pedidoFinal = [];
 let cuentaFinal = 0;
 let clientes = prompt("¿Cuántos van a comer?")
-let pedido = prompt("Ingrese el número de su pedido: 1: Cerveza 2: Fernet 3: Papas Fritas");
+//let pedido = prompt("Ingrese el número de su pedido: 1: Cerveza 2: Fernet 3: Papas Fritas");
+let tabla = document.getElementById("productos");
+let botonConfirmar = document.getElementById("btn-confirmar")
+const productos = [
+    { name: 'Cerveza', costo: 300, id: 'btn-cerveza' },
+    { name: 'Fernet', costo: 400, id: 'btn-fernet' },
+    { name: 'Papas Fritas', costo: 350, id: 'btn-papas' }];
+
 
 function agregarProductoAlPedido(producto) {
-    alert(`Elegiste ${producto.name}. Costo ${producto.costo}. Para finalizar ingresa ESC`)
+    alert(`Elegiste ${producto.name}. Costo ${producto.costo}.`)
     pedidoFinal.push(producto);
     cuentaFinal += producto.costo;
 }
 
-let tabla = document.getElementById("productos");
+
 function agregarItemHTML(producto) {
     let row = document.createElement("tr");
     row.innerHTML = `<tr><th>${producto.name}</th><th>${producto.costo}</th></tr>`;
@@ -23,37 +30,26 @@ function crearProducto() {
     return new producto(nombre, precio);
 }
 
-//let boton = document.getElementById("btn-cerveza")
-    //  boton.onclick = () =>{agregarProductoAlPedido({ name: 'Cerveza', costo: 300 });
 
 
-while (pedido != "esc") {
-    switch (pedido) {
-        case "1":
-            agregarProductoAlPedido({ name: 'Cerveza', costo: 300 });
-            break;
-        case "2":
-            agregarProductoAlPedido({ name: 'Fernet', costo: 400 });
-
-            break;
-        case "3":
-            agregarProductoAlPedido({ name: 'Papas Fritas', costo: 350 });
-
-            break;
-        default:
-            alert("No tenemos esta opción, intenta otra vez.")
-            break;
-    }
-    pedido = prompt("Ingrese otro pedido: 1: Cerveza 2: Fernet 3: Papas Fritas");
-}
 //Cuenta final por cada uno
-let cadaUno = cuentaFinal / clientes;
 
-document.getElementById("total").innerText = `El total es ${cuentaFinal}`;
 
-document.getElementById("romana").innerText = `Cada uno paga ${cadaUno}`;
 
-pedidoFinal.forEach((producto) => {
-    agregarItemHTML(producto);
-});
 
+function confirmarPedido() {
+    pedidoFinal.forEach((producto) => {
+        agregarItemHTML(producto);
+        let cadaUno = cuentaFinal / clientes;
+
+        document.getElementById("total").innerText = `El total es ${cuentaFinal}`;
+
+        document.getElementById("romana").innerText = `Cada uno paga ${cadaUno}`;
+    });
+}
+botonConfirmar.onclick = confirmarPedido;
+
+productos.forEach((producto) => {
+    let boton = document.getElementById(producto.id);
+    boton.onclick = () => { agregarProductoAlPedido(producto); };
+})
